985841d2847919522f790010e9d6306d677b5f4b
Gregor Olenik <gregor.olenik@web.de>
2025-05-02 16:12:28 +0200

    update neofoam package

--- var/spack/repos/builtin/packages/neofoam/package.py
+++ var/spack/repos/builtin/packages/neofoam/package.py
@@ -4,20 +4,34 @@
 
 from spack.package import *
 
-
 class Neofoam(CMakePackage):
     """NeoFOAM is a WIP prototype of a modern CFD core."""
 
+    license("GPL-3.0-or-later")
+
     homepage = "https://github.com/exasim-project/NeoFOAM"
-    git = "https://github.com/exasim-project/NeoFOAM.git"
+    git = "https://github.com/exasim-project/FoamAdapter.git"
 
     maintainers("greole", "HenningScheufler")
 
-    license("MIT", checked_by="greole")
-
     version("main", branch="main")
+
+    variant("cuda", default=False, description="Compile with CUDA support")
+    variant("hip", default=False, description="Compile with HIP support")
+    variant("test", default=False, description="")
+    variant("examples", default=False, description="")
+    variant("benchmarks", default=False, description="")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
-    depends_on("mpi")
-    depends_on("kokkos@4.3.0")
+    depends_on("neon")
+    depends_on("openfoam")
+
+    def cmake_args(self):
+        return [
+            '-DFOAMADAPTER_BUILD_TESTS=%s' % ('+test' in self.spec),
+            '-DFOAMADAPTER_BUILD_EXAMPLES=%s' % ('+examples' in self.spec),
+            '-DFOAMADAPTER_BUILD_BENCHMARKS=%s' % ('+benchmarks' in self.spec),
+            '-DKokkos_ENABLE_CUDA=%s' % ('+cuda' in self.spec),
+            '-DKokkos_ENABLE_HIP=%s' % ('+hip' in self.spec),
+        ]
